60bbcbcaf36a2aceff09f9e830ff9eb2d4cf5d5f
Ravi Mehta <rvmehta745@gmail.com>
2025-09-09 23:29:48 +0530

    Change me api response

--- app/Http/Controllers/V1/Admin/LoginController.php
+++ app/Http/Controllers/V1/Admin/LoginController.php
@@ -616,7 +616,7 @@ class LoginController extends BaseController
 
             // Get max sales volume from manage_contact table
             // Add max sales volume to the response
-            return General::setResponse("SUCCESS", ['NOTIFICATION' => ['User data fetched successfully']], [
+            return General::setResponse("SUCCESS", ["User data fetched successfully"], [
                 'user' => $user
             ]);
         } catch (Throwable $e) {
