90cf2fce23732d93e3beb9e54356f6a4e08f9db8
Ravi Mehta <rvmehta745@gmail.com>
2025-09-20 14:49:52 +0530

    Generate Menu API

--- app/Http/Controllers/V1/Admin/LoginController.php
+++ app/Http/Controllers/V1/Admin/LoginController.php
@@ -616,7 +616,7 @@ class LoginController extends BaseController
 
             // Get max sales volume from manage_contact table
             // Add max sales volume to the response
-            return General::setResponse("SUCCESS", ["User data fetched successfully"], [
+            return General::setResponse("SUCCESS", [], [
                 'user' => $user
             ]);
         } catch (Throwable $e) {
